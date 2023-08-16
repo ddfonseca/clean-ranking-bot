063f1cd35e74c8dd99e707d04f8121d670c848b0
David Fonseca <olfdavid@gmail.com>
2023-08-16 08:17:42 -0300

    fix: :bug: left chat participant

--- src/infra/http/http.controller.ts
+++ src/infra/http/http.controller.ts
@@ -5,13 +5,18 @@ import { UsecaseFactory } from '../../application/factory/usecase.factory'
 export default class HttpController {
 	constructor(httpServer: HttpServer, usecaseFactory: UsecaseFactory) {
 		httpServer.on('post', '/bot', async function (params: any, body: any, headers: any) {
-			console.log(body)
 			if (body?.edited_message) return
+			if (body?.left_chat_participant) return
 			const sentMessage = body?.message?.text
 			if (sentMessage.match(/\/add/gi)) {
 				const addHour = usecaseFactory.createAddHour()
 				await addHour.execute(body)
+				return
 			}
+		})
+
+		httpServer.on('get', '/', async function (params: any, body: any, headers: any) {
+			return 'Hello world'
 		})
 	}
 }
